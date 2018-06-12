2ecfc5a360ee359c1e378c2012b5a9f42658ee9b
John MacTavish <john@techsolx.com>
2018-06-12 23:28:20 +0000

    Formatting the layout.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+@app.route('/members')
+def members():
+    return 'The members page'
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
